dd4fd525e7cce9202e786a2aee4cc4b9e54c20cd
Lucidevz <114227439+s4203133@users.noreply.github.com>
2025-01-29 09:42:27 +0000

    Improved Camera Shake System
    used online resources to discover ways of 'juicing' a game camera using mathematical concepts. The structure of the camera shake class has been changed to allow camera shakes to be stacked on top of each other for an increasing effect. The shake also smoothly transitions out, and extra parameters to control the max yaw, pitch, roll and translational movement have been added. Refactored the class to make the code cleaner and easier to setup camera shakes with different events.

--- Assets/Scripts/Camera/CameraShake.cs
+++ Assets/Scripts/Camera/CameraShake.cs
@@ -7,25 +7,6 @@ public class CameraShake : ScriptableObject
     [SerializeField] private float duration;
     [SerializeField] private float magnitude;
 
-    public IEnumerator ShakeCamera(Transform camTransform) {
-        Vector3 originalPosition = camTransform.localPosition;
-        float t = 0;
-        while(t < duration) {
-            float x = Random.Range(-magnitude, magnitude);
-            float y = Random.Range(-magnitude, magnitude);
-
-            Vector3 newPosition = originalPosition + new Vector3(x, 0, y);
-            camTransform.localPosition = newPosition;
-
-            t += Time.deltaTime;
-
-            yield return null;
-        }
-    }
+    public float Duration => duration;
+    public float Magnitude => magnitude;    
 }
-
-[System.Serializable]
-public class CameraShakeComponent {
-    [SerializeField] private CameraShake cameraShake;
-}
-
--- Assets/Scripts/Camera/CameraShaker.cs
+++ Assets/Scripts/Camera/CameraShaker.cs
@@ -1,63 +1,184 @@
+using System.Runtime.CompilerServices;
+using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
 
 public class CameraShaker : MonoBehaviour
 {
-    [SerializeField] private Camera _camera;
-    private Coroutine currentShake;
+    [SerializeField] private Transform _camera;
+    [SerializeField] private Transform cameraParent;
 
+    [SerializeField] private float maxYaw;
+    [SerializeField] private float maxPitch;
+    [SerializeField] private float maxRoll;
+    [SerializeField] private float maxPositionOffset;
+
+    [Header("CAMERA SHAKES")]
     [SerializeField] private CameraShakes cameraShakes;
- 
+
+    private float duration;
+    private float magnitude;
+    private float intensity;
+
     private void OnEnable() {
-        SubscribeSmallCameraShakes();
+        cameraShakes.SubscribeCameraShakeActions();
+        SubscribeCameraShakes();
     }
 
     private void OnDisable() {
-        UnsubscribeSmallCameraShakes();
+        cameraShakes.UnsubscribeCameraShakeActions();
+        UnsubscribeCameraShakes();
+    }
+
+    private void Update() {
+        ReduceShake();
+        Shake();
     }
 
     private void ShakeCamera(CameraShake shake) {
-        if(currentShake != null) {
-            StopCoroutine(currentShake);
+        duration += shake.Duration;
+        intensity += shake.Magnitude;
+        duration = Mathf.Clamp(duration, 0.0f, 1.0f);
+        intensity = Mathf.Clamp(intensity, 0.0f, 1.0f);
+    }
+
+    private void Shake() {
+        magnitude = Mathf.Pow(intensity, 2);
+
+        float seed = Time.time;
+        Vector3 newPosition = GetTranslationalShake(seed);
+        Quaternion newRotation = GetRotationalShake(seed);
+
+        ApplyCameraShake(newPosition, newRotation);
+    }
+
+    private void ReduceShake() {
+        duration -= Time.deltaTime;
+        if(duration < 0) { 
+            duration = 0;
+            ResetCamera();
         }
-        currentShake = StartCoroutine(shake.ShakeCamera(_camera.transform));
+
+        intensity -= Time.deltaTime;
+        if(intensity < 0) {
+            intensity = 0; 
+        }
     }
 
-    // Small Camera Shakes
-
-    private void SmallCameraShake() {
-        ShakeCamera(cameraShakes.small);
+    private Vector3 GetTranslationalShake(float seed) {
+        float xOffset = maxPositionOffset * magnitude * Mathf.PerlinNoise(seed + 3, seed + 3) * RandomMultiplier();
+        float yOffset = maxPositionOffset * magnitude * Mathf.PerlinNoise(seed + 4, seed + 4) * RandomMultiplier();
+        float zOffset = maxPositionOffset * magnitude * Mathf.PerlinNoise(seed + 5, seed + 5) * RandomMultiplier();
+        return new Vector3(xOffset, yOffset, zOffset);
     }
 
-    private void SubscribeSmallCameraShakes() {
-        PlayerGroundPoundState.OnLanded += SmallCameraShake;
-        IBreakable.OnBroken += SmallCameraShake;
+    private Quaternion GetRotationalShake(float seed) {
+        float yaw = maxYaw * magnitude * Mathf.PerlinNoise(seed, seed) * RandomMultiplier();
+        float pitch = maxPitch * magnitude * Mathf.PerlinNoise(seed + 1, seed + 1) * RandomMultiplier();
+        float roll = maxRoll * magnitude * Mathf.PerlinNoise(seed + 2, seed + 2) * RandomMultiplier();
+        return Quaternion.Euler(yaw, pitch, roll);
     }
 
-    private void UnsubscribeSmallCameraShakes() {
-        PlayerGroundPoundState.OnLanded += SmallCameraShake;
-        IBreakable.OnBroken -= SmallCameraShake;
+    private void ApplyCameraShake(Vector3 translation, Quaternion rotation) {
+        _camera.localPosition = translation;
+        _camera.localRotation = rotation;
     }
 
-    // Medium Camera Shakes
-
-    private void MediumCameraShake() {
-        ShakeCamera(cameraShakes.medium);
+    private float RandomMultiplier() {
+        return Random.Range(-1f, 1f);
     }
 
-    // Large Camera Shakes
+    private void ResetCamera() {
+        _camera.localRotation = Quaternion.Euler(Vector3.zero);
+        _camera.localPosition = Vector3.zero;
+    }
 
-    private void LargeCameraShake() {
-        ShakeCamera(cameraShakes.Large);
+    private void SubscribeCameraShakes() {
+        cameraShakes.small.OnShake += ShakeCamera;
+        cameraShakes.medium.OnShake += ShakeCamera;
+        cameraShakes.large.OnShake += ShakeCamera;
+    }
+
+    private void UnsubscribeCameraShakes() {
+        cameraShakes.small.OnShake -= ShakeCamera;
+        cameraShakes.medium.OnShake -= ShakeCamera;
+        cameraShakes.large.OnShake -= ShakeCamera;
+    }
+}
+
+
+
+[System.Serializable]
+public class CameraShakeType {
+    [SerializeField] protected CameraShake camShake;
+    public delegate void CustomEvent(CameraShake shakey);
+    public CustomEvent OnShake;
+
+    public virtual void SubscribeToActions() {
+
+    }
+
+    public virtual void UnsubscribeToActions() {
+
+    }
+
+    protected void Shake() {
+        OnShake?.Invoke(camShake);
+    }
+}
+
+[System.Serializable]
+public class SmallCameraShake : CameraShakeType {
+
+    public override void SubscribeToActions() {
+        PlayerGroundPoundState.OnLanded += Shake;
+        IBreakable.OnBroken += Shake;
+    }
+
+    public override void UnsubscribeToActions() {
+        PlayerGroundPoundState.OnLanded += Shake;
+        IBreakable.OnBroken -= Shake;
+    }
+}
+
+[System.Serializable]
+public class MediumCameraShake : CameraShakeType {
+
+    public override void SubscribeToActions() {
+    }
+
+    public override void UnsubscribeToActions() {
+    }
+}
+
+[System.Serializable]
+public class LargeCameraShake : CameraShakeType {
+
+    public override void SubscribeToActions() {
+    }
+
+    public override void UnsubscribeToActions() {
     }
 }
 
 [System.Serializable]
 public class CameraShakes {
-    [SerializeField] private CameraShake smallCameraShake;
-    [SerializeField] private CameraShake mediumCameraShake;
-    [SerializeField] private CameraShake LargeCameraShake;
+    [SerializeField] private SmallCameraShake smallCameraShake;
+    [SerializeField] private MediumCameraShake mediumCameraShake;
+    [SerializeField] private LargeCameraShake largeCameraShake;
 
-    public CameraShake small => smallCameraShake;
-    public CameraShake medium => mediumCameraShake;
-    public CameraShake Large => LargeCameraShake;
+    public SmallCameraShake small => smallCameraShake;
+    public MediumCameraShake medium => mediumCameraShake;
+    public LargeCameraShake large => largeCameraShake;
+
+    public void SubscribeCameraShakeActions() {
+        smallCameraShake.SubscribeToActions();
+        mediumCameraShake.SubscribeToActions();
+        largeCameraShake.SubscribeToActions();
+    }
+
+    public void UnsubscribeCameraShakeActions() {
+        smallCameraShake.UnsubscribeToActions();
+        mediumCameraShake.UnsubscribeToActions();
+        largeCameraShake.UnsubscribeToActions();
+    }
 }
