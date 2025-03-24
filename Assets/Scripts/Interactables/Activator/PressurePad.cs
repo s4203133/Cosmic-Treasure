2f5dc80068978333c7ff4015fdf78098d774cc97
Nate <nate.rawnsley@gmail.com>
2025-03-24 19:01:00 +0000

    Adjusted pressure pad

--- Assets/Scripts/Interactables/Activator/PressurePad.cs
+++ Assets/Scripts/Interactables/Activator/PressurePad.cs
@@ -2,9 +2,21 @@ using LMO;
 
 namespace NR {
     public class PressurePad : Activator, ICrushable {
+        private bool buttonReady = true;
+
         public void OnHit() {
-            OnActivate?.Invoke();
-            animator.SetTrigger("Hit");
+            if (buttonReady) {
+                OnActivate?.Invoke();
+                animator.SetBool("Hit", true);
+                buttonReady = false;
+            }
+        }
+
+        //Called from the animation on the button.
+        //This ensures the button isn't triggered repeatedly in quick succession
+        public void ButtonReady() {
+            animator.SetBool("Hit", false);
+            buttonReady = true;
         }
     }
 }
