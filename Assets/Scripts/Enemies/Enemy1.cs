48da51c2e2e654c6d1e33d6325129892329ca0fd
willwhop <146436965+willwhop@users.noreply.github.com>
2025-03-26 17:25:51 +0000

    basic enemy movement added

---
+++ Assets/Scripts/Enemies/Enemy1.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy1 : MonoBehaviour
+{
+    public List<Vector3> Rays;
+    public float distance;
+    public NavMeshAgent Enemy;
+    public List<Transform> PatrolPoints;
+    private int PointIteration;
+    private Transform CurrentPoint;
+    private bool patrol;
+    public bool IsAtEnd;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsAtEnd = false;
+    }
+    void RayDirection() {
+        foreach (Vector3 ray in Rays) {
+            RaycastHit hit = new RaycastHit();
+            Vector3 angle = new Vector3(0, 0, 0) + Quaternion.Euler(ray.x, ray.y, ray.z) * transform.forward;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(angle), out hit, distance)) {
+                Debug.DrawRay(transform.position, transform.TransformDirection(angle) * distance, Color.red);
+                if (hit.collider.CompareTag("Player")) {
+                    if (hit.distance > 1) {
+                        Enemy.SetDestination(hit.collider.transform.position);
+                        transform.LookAt(hit.transform.position);
+                        //lerp to look at the player. if player out of sight get last location. if not there then reset
+                    }
+                    if (hit.distance <= 1) {
+                        //play attack animation
+                    }
+
+
+                    
+                }
+            }
+            else {
+                foreach (Transform point in PatrolPoints) {
+                    if (point == PatrolPoints[PointIteration]) {
+                        if (transform.position != point.transform.position) {
+                            Enemy.SetDestination(point.transform.position);
+                        }
+                        
+                        CurrentPoint = point;
+                        if (transform.position.x >= CurrentPoint.position.x) {
+                            if (transform.position.y >= CurrentPoint.position.y) {
+                                if (transform.position.z >= CurrentPoint.position.z) {
+                                    timer += Time.deltaTime;
+                                    float rand = UnityEngine.Random.Range(2, 7);
+                                    if (timer >= rand) {
+                                        PointIteration += 1;
+                                        timer = 0;
+                                    }
+                                    
+                                }
+                            }
+                        }                        
+                    }
+                    
+
+                }
+                if (PointIteration >= PatrolPoints.Count ) {
+                    PointIteration = 0;
+                    IsAtEnd = false;
+                }
+            }
+            Debug.Log(timer);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        RayDirection();
+
+    }
+}
