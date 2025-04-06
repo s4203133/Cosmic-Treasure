34d97ad78be61b48b582eb5efb043ea6569944f8
Lucidevz <114227439+s4203133@users.noreply.github.com>
2025-04-06 21:04:10 +0100

    Small Player Control Tweaks
    Adjusted the speed at which the player can move while they are spin attacking, as well as the "Landing Time" of the ground pound mechanic.

--- Assets/Scripts/Player/Utilities/PlayerSettings.cs
+++ Assets/Scripts/Player/Utilities/PlayerSettings.cs
@@ -8,6 +8,9 @@ namespace LMO {
         [SerializeField] private PlayerMovementSettings movementSettings;
         public PlayerMovementSettings Movement => movementSettings;
 
+        [SerializeField] private PlayerMovementSettings spinMovement;
+        public PlayerMovementSettings SpinAttack => spinMovement;
+
         [SerializeField] private PlayerMovementSettings hoverMoveSettings;
         public PlayerMovementSettings HoverMovement => hoverMoveSettings;
 
