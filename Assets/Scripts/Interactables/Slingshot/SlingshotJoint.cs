77b08f02187dcc158df189bf14a0e21a5b4285f7
Nate <nate.rawnsley@gmail.com>
2025-04-18 16:02:44 +0100

    Added line renderer to slingshots

--- Assets/Scripts/Interactables/Slingshot/SlingshotJoint.cs
+++ Assets/Scripts/Interactables/Slingshot/SlingshotJoint.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections;
 
 namespace NR {
-
+    [RequireComponent(typeof(LineRenderer))]
     public class SlingshotJoint : GrapplePoint {
         private Transform slingshotTransform;
         public Transform SlingshotTransform => slingshotTransform;
@@ -12,8 +12,13 @@ namespace NR {
         private Vector3 playerDragPos;
         public Vector3 PlayerDragPosition => playerDragPos;
 
+        [SerializeField]
+        private Transform[] linePositions;
+        private Vector3[] linePoints;
+
         private Rigidbody rb;
         private SlingshotDragger playerDrag;
+        private LineRenderer lr;
         public Action SlingshotUpdate;
         public Action SlingshotReleased;
 
@@ -21,8 +26,11 @@ namespace NR {
         protected override void Start() {
             base.Start();
             rb = GetComponent<Rigidbody>();
+            lr = GetComponent<LineRenderer>();
             playerDrag = FindObjectOfType<SlingshotDragger>();
             interactActions.AddListener(playerDrag.DisconnectSlingshot);
+            lr.positionCount = linePositions.Length;
+            linePoints = new Vector3[linePositions.Length];
         }
 
         public void SetParentTransform(Transform slingTransform) {
@@ -38,6 +46,15 @@ namespace NR {
             StopCoroutine("UpdatePosition");
             playerDrag.DisconnectSlingshot();
             SlingshotReleased?.Invoke();
+        }
+
+        private void LateUpdate() {
+            if (linePositions.Length > 0) {
+                for (int i = 0; i < linePoints.Length; i++) {
+                    linePoints[i] = linePositions[i].position;
+                }
+                lr.SetPositions(linePoints);
+            }
         }
 
         private IEnumerator UpdatePosition() {
