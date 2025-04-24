eaea5154480b63c6f2c5d18cbcd8c21498a8da9e
willwhop <146436965+willwhop@users.noreply.github.com>
2025-04-24 20:09:31 +0100

    added basic pause menu with resolutions and basic keyboard controls
    tried to add a virtual mouse and gamepad controls for it but didn't work

---
+++ Assets/Scripts/Pause Menu (Will)/CustomButtonTriggerBox.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomButtonTriggerBox : MonoBehaviour {
+    private Button ThisButton;
+    //private void Start() {
+    //    ThisButton = GetComponent<Button>();
+    //}
+    
+    //private void OnTriggerEnter(Collider other) {
+    //    if (other.CompareTag("Pointer")) {
+    //        Debug.Log("hovering");
+    //        if (FindObjectOfType<PauseMenu>().clicked.IsPressed()) {
+    //            ThisButton.onClick.Invoke();
+    //            Debug.Log("clicked");
+    //        }            
+    //    }
+    //}
+}
