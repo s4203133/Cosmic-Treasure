9cd8e26860c9a401c680ce9b12136f4602f7a51f
Lucidevz <114227439+s4203133@users.noreply.github.com>
2025-05-26 18:59:01 +0100

    Fixed Bugs

--- Assets/Scripts/Mini_Games/TorchController.cs
+++ Assets/Scripts/Mini_Games/TorchController.cs
@@ -1,27 +1,31 @@
 using UnityEngine;
 
-public class PuzzleManager : MonoBehaviour
+public class TorchController : MonoBehaviour
 {
-    public static PuzzleManager Instance;
+    //public static PuzzleManager Instance;
+    //
+    //public TorchController[] torches;
+    //public int[] correctPattern;
+    //public GameObject rabbitCage;
+    //
+    //private void Awake()
+    //{
+    //    Instance = this;
+    //}
+    //
+    //public void CheckCombination()
+    //{
+    //    for (int i = 0; i < torches.Length; i++)
+    //    {
+    //        if (torches[i].GetColorIndex() != correctPattern[i])
+    //            return;
+    //    }
+    //
+    //    Debug.Log("Puzzle Solved!");
+    //    rabbitCage.SetActive(false); // Or trigger animation here
+    //}
 
-    public TorchController[] torches;
-    public int[] correctPattern;
-    public GameObject rabbitCage;
-
-    private void Awake()
-    {
-        Instance = this;
-    }
-
-    public void CheckCombination()
-    {
-        for (int i = 0; i < torches.Length; i++)
-        {
-            if (torches[i].GetColorIndex() != correctPattern[i])
-                return;
-        }
-
-        Debug.Log("Puzzle Solved!");
-        rabbitCage.SetActive(false); // Or trigger animation here
+    public int GetColorIndex() {
+        return 0;
     }
 }
