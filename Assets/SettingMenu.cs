e9070ec4a235bae5a710c2b711cf952d9eb2af3d
Charlotte Keane <116003044+CharlotteKeane18@users.noreply.github.com>
2025-05-27 00:57:38 +0100

    Create Setting Options

--- Assets/SettingMenu.cs
+++ Assets/SettingMenu.cs
@@ -3,13 +3,51 @@ using System.Collections.Generic;
 using System.Xml.Linq;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour {
 
-    // Volume Options
 
     public AudioMixer audioMixer;
+    public Dropdown resolutionDropdown;
 
+    //Resolution Options
+
+    Resolution[] resolutions;
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List.<string> LoadOptions = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions [i].width + "x" + resolutions[i].height;
+            Options.Add(option);
+
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height== Screen.currentResolution.height) 
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        
+        resolutionsDropdown.AppOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    { 
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    // Volume Options
     public void SetVolume (float volume)
     {
         audioMixer.SetFloat("volume", volume);
@@ -22,5 +60,14 @@ public class SettingsMenu : MonoBehaviour {
     {
         QualitySettings.SetQualityLevel(qualityIndex);
     }
-    
+
+    // Fullscreen Option
+
+    public void SetFullscreen (bool isFullscreen)
+    { 
+         Screen.fullScreen = isFullscreen;
+    }
+
+
+
 }
